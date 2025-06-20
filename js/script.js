b61f1aa5d9716546c0269cc72f98ce488defaddc
Imbert97 <77212776+Imbert97@users.noreply.github.com>
2025-06-20 17:57:07 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Variables globales
+let map, marker, lat, lng, weatherData = null;
+
+// Codes météo WMO
+const weatherCodes = {
+    0: "Ciel dégagé",
+    1: "Principalement dégagé",
+    // ... tout votre JavaScript existant
+};
+
+// Toutes vos fonctions existantes
+function initMap() {
+    // ... votre code
+}
